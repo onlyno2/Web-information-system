89bf8742e3b315d2368cdd75260decaa841d48f7
Nguyen Kien <kiennd010498@gmail.com>
2019-12-16 17:29:00 +0700

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2019_12_16_095831) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
